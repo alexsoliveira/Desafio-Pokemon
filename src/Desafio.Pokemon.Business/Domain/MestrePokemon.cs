bebebcb9f9ac302884a11105a8b1d05a938508d7
alexsoliveira <alexsoliveira@gmail.com>
2024-09-06 15:09:46 -0300

    Altaracao Models para Domain

---
+++ src/Desafio.Pokemon.Business/Domain/MestrePokemon.cs
@@ -0,0 +1,18 @@
+﻿namespace Desafio.Pokemon.Business.Domain
+{
+    public class MestrePokemon
+    {        
+        public Guid Id { get; private set; }
+        public string Nome { get; private set; }
+        public byte Idade { get; private set; }
+        public CPF Cpf { get; private set ; }
+
+        public MestrePokemon(Guid id, string nome, byte idade, CPF cpf)
+        {
+            Id = id;
+            Nome = nome;
+            Idade = idade;
+            Cpf = cpf;
+        }
+    }
+}
