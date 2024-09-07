6e4c8d01e076cb01c4f8135516a99f003e404376
alexsoliveira <alexsoliveira@gmail.com>
2024-09-07 11:40:55 -0300

    Mudar o nome da pasta Models para Domain

---
+++ src/Desafio.Pokemon.Business/Domain/CPF.cs
@@ -0,0 +1,19 @@
+﻿namespace Desafio.Pokemon.Business.Domain
+{
+    public class CPF
+    {
+        public string Numero { get; set; }
+
+        public static bool Validar(string cpf)
+        {
+            var cpfValido = true;
+
+            if (string.IsNullOrWhiteSpace(cpf))
+            {
+                return false; 
+            }
+
+            return cpfValido;
+        }
+    }
+}
---
+++ src/Desafio.Pokemon.Business/Domain/MestrePokemon.cs
@@ -0,0 +1,30 @@
+﻿using Desafio.Pokemon.Business.Validations;
+using System.Xml.Linq;
+
+namespace Desafio.Pokemon.Business.Domain
+{
+    public class MestrePokemon
+    {        
+        public Guid Id { get; private set; }
+        public string Nome { get; private set; }
+        public byte Idade { get; private set; }
+        public CPF Cpf { get; private set ; }
+
+        public MestrePokemon(string nome, byte idade, CPF cpf)
+        {            
+            Nome = nome;
+            Idade = idade;
+            Cpf = cpf;
+        }
+
+        private void Validate()
+        {
+            DomainValidation.NotNullOrEmpty(Nome, nameof(Nome));
+            DomainValidation.MinLength(Nome, 3, nameof(Nome));
+            DomainValidation.MaxLength(Nome, 30, nameof(Nome));
+
+            DomainValidation.MinLength(Idade, 10, nameof(Idade));
+            DomainValidation.MaxLength(Idade, 100, nameof(Idade));
+        }
+    }
+}
