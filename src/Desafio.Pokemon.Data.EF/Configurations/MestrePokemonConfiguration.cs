e43b735740b2c36313f79d9391a90d2f3a0d9514
alexsoliveira <alexsoliveira@gmail.com>
2024-09-10 20:31:42 -0300

    Inclusao do contexto do banco e seu repositorio mestre pokemon

---
+++ src/Desafio.Pokemon.Data.EF/Configurations/MestrePokemonConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using Desafio.Pokemon.Business.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Reflection.Emit;
+
+namespace Desafio.Pokemon.Data.EF.Configurations
+{
+    internal class MestrePokemonConfiguration 
+        : IEntityTypeConfiguration<MestrePokemon> 
+    {
+        public void Configure(EntityTypeBuilder<MestrePokemon> builder)
+        {
+            builder.HasKey(mestrePokemon => mestrePokemon.Id);
+
+            builder.Property(mestrePokemon => mestrePokemon.Nome)
+                .HasMaxLength(30);
+
+            builder.Property(mestrePokemon => mestrePokemon.Idade)
+                .HasMaxLength(3);
+
+            //builder.Property(mestrePokemon => mestrePokemon.Cpf)
+            //    .HasMaxLength(11);
+
+            builder.OwnsOne(mestrePokemon => mestrePokemon.Cpf, cpf =>
+                cpf.Property(campo => campo.Numero)
+                .HasColumnName("Cpf")
+                .HasMaxLength(11)
+            );
+
+            //builder.OwnsOne<Cpf>("Cpf");
+
+            //builder.OwnsOne(m => m.Cpf, c =>
+            //{
+            //    c.Property(x => x.Numero).HasColumnName("Cpf");
+            //});
+        }
+    }
+}
---
+++ src/Desafio.Pokemon.Data.EF/Repositories/MestrePokemonRepository.cs
@@ -0,0 +1,20 @@
+﻿using Desafio.Pokemon.Business.Domain;
+using Desafio.Pokemon.Business.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Desafio.Pokemon.Data.EF.Repositories
+{
+    public class MestrePokemonRepository : IMestrePokemonRepository<MestrePokemon>
+    {
+        private readonly DesafioPokemonDbContext _context;        
+
+        private DbSet<MestrePokemon> _mestresPokemon
+            => _context.Set<MestrePokemon>();
+
+        public MestrePokemonRepository(DesafioPokemonDbContext context)
+            => _context = context;
+
+        public async Task Insert(MestrePokemon aggregate)
+            => await _mestresPokemon.AddAsync(aggregate);        
+    }
+}
