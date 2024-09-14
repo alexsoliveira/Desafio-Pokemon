6c612c23ed853044d4130fc0e56ddb6705c71bc5
alexsoliveira <alexsoliveira@gmail.com>
2024-09-14 11:28:00 -0300

    Criacao do teste do pokemon service

---
+++ tests/Desafio.Pokemon.IntegrationTests/Services/Pokemon/PokemonServiceTestFixture.cs
@@ -0,0 +1,31 @@
+﻿namespace Desafio.Pokemon.IntegrationTestsApi.Pokemon
+{
+    [CollectionDefinition(nameof(PokemonServiceTestFixture))]
+    public class PokemonServiceTestFixtureCollection
+        : ICollectionFixture<PokemonServiceTestFixture>
+    { }
+
+    public class PokemonServiceTestFixture
+    {
+        public PokemonServiceTestFixture()
+        { }
+
+        public int ObterIdPrimeiraGeracaoPokemonValido()
+        {
+            var id = 0;
+
+            id = new Random().Next(1, 151);
+
+            return id;
+        }
+
+        public int ObterIdPrimeiraGeracaoPokemonInvalido()
+        {
+            var id = 0;
+            
+            id = new Random().Next(152, 1000);
+
+            return id;
+        }
+    }
+}
