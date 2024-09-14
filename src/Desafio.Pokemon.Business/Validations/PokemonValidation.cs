84d7eda659efee7e73fa8faaeed723a0cf451815
alexsoliveira <alexsoliveira@gmail.com>
2024-09-14 11:29:17 -0300

    Craicao da validatacao pokemon service

---
+++ src/Desafio.Pokemon.Business/Validations/PokemonValidation.cs
@@ -0,0 +1,21 @@
+﻿using Desafio.Pokemon.Business.Exceptions;
+
+namespace Desafio.Pokemon.Business.Validations
+{
+    public static class PokemonValidation
+    {
+        public static void MinLength(int target, int minLength, string fieldName)
+        {
+            if (target < minLength)
+                throw new EntityValidationException(
+                    $"{fieldName} deve ser maior ou igual a {minLength} pokemon");
+        }
+
+        public static void MaxLength(int target, int maxLength, string fieldName)
+        {
+            if (target > maxLength)
+                throw new EntityValidationException(
+                    $"{fieldName} deve ser menor ou igual a {maxLength} pokemon");
+        }
+    }
+}
