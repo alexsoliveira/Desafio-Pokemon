8de8783798ea3cf049d9249f212f34cdf2d1378e
alexsoliveira <alexsoliveira@gmail.com>
2024-09-14 11:28:25 -0300

    Criacao da interface pokemon service

--- src/Desafio.Pokemon.Business/Interfaces/IPokemonService.cs
+++ src/Desafio.Pokemon.Business/Interfaces/IPokemonService.cs
@@ -1,10 +1,16 @@
 ﻿using Desafio.Pokemon.Business.Domain.Pokemon;
+using Desafio.Pokemon.Business.Domain.Pokemon.Evolucao;
+using Desafio.Pokemon.Business.Domain.Pokemon.Evolucao.CadeiaEvolucao;
 
 namespace Desafio.Pokemon.Business.Interfaces
 {
     public interface IPokemonService
     {
         Task<(HttpResponseMessage?, List<PokemonDetalhes>?)> ObterPokemons();
-        Task<(HttpResponseMessage?, PokemonDetalhes?)> ObterPorId(int id);        
+        Task<(HttpResponseMessage?, PokemonDetalhes?)> ObterPokemonPorId(int id);
+
+        Task<(HttpResponseMessage?, PokemonDetalhes?)> ObterDetalhesPokemonPorId(int id);
+        Task<(HttpResponseMessage?, PokemonEvolucao?)> ObterEvolucaoPokemonPorId(int id);
+        Task<(HttpResponseMessage?, CadeiaEvolucao?)> ObterCadeiaEvolucaoPokemonPorId(int id);
     }
 }
