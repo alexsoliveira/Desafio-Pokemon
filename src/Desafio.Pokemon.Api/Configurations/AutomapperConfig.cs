8b17e45998b0781b18dbaeeeeae188668e0b83ff
alexsoliveira <alexsoliveira@gmail.com>
2024-09-15 12:25:08 -0300

    Ajuste no automapper e injecao de dependencia e inclusao dos controller pokemon com dois metodos obterpokemons e obterpokemonporId

--- src/Desafio.Pokemon.Api/Configurations/AutomapperConfig.cs
+++ src/Desafio.Pokemon.Api/Configurations/AutomapperConfig.cs
@@ -1,6 +1,10 @@
 ﻿using AutoMapper;
 using Desafio.Pokemon.Api.ViewModels;
+using Desafio.Pokemon.Api.ViewModels.Pokemon;
+using Desafio.Pokemon.Api.ViewModels.Pokemon.CadeiaEvolucao;
 using Desafio.Pokemon.Business.Domain;
+using Desafio.Pokemon.Business.Domain.Pokemon;
+using Desafio.Pokemon.Business.Domain.Pokemon.Evolucao.CadeiaEvolucao;
 
 namespace Desafio.Pokemon.Api.Configurations
 {
@@ -10,6 +14,16 @@ namespace Desafio.Pokemon.Api.Configurations
         {
             CreateMap<MestrePokemon, MestrePokemonViewModel>().ReverseMap();                   
             CreateMap<Cpf, CpfViewModel>().ReverseMap();
+
+            CreateMap<PokemonDetalhes, PokemonDetalhesViewModel>().ReverseMap();
+            CreateMap<PokemonSprites, PokemonSpritesViewModel>().ReverseMap();
+            CreateMap<PokemonType, PokemonTypeViewModel>().ReverseMap();
+            CreateMap<Tipo, TypeViewModel>().ReverseMap();
+
+            CreateMap<CadeiaEvolucao, CadeiaEvolucaoViewModel>().ReverseMap();
+            CreateMap<Chain,  ChainViewModel>().ReverseMap();
+            CreateMap<EvolvesTo, EvolvesToViewModel>().ReverseMap();
+            CreateMap<Species, SpeciesViewModel>().ReverseMap();
         }
     }
 }
---
+++ src/Desafio.Pokemon.Api/Controllers/PokemonsController.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using Desafio.Pokemon.Api.ViewModels.Pokemon;
+using Desafio.Pokemon.Business.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Desafio.Pokemon.Api.Controllers
+{
+    [Route("api/v1/pokemon")]
+    public class PokemonsController : MainController
+    {
+        private readonly IPokemonService _pokemonService;
+        private readonly IMapper _mapper;
+
+        public PokemonsController(
+            IPokemonService pokemonService,
+            IMapper mapper,
+            INotificador notificador) : base(notificador)
+        {
+            _pokemonService = pokemonService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(PokemonDetalhesViewModel), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status422UnprocessableEntity)]
+        public async Task<IActionResult> ObterPokemons()
+        {                                                 
+            var resultado = _mapper.Map<IEnumerable<PokemonDetalhesViewModel>>(await _pokemonService.ObterPokemons());
+
+            return CustomResponse(HttpStatusCode.OK, new { resultado });
+        }
+
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(typeof(PokemonDetalhesViewModel), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status422UnprocessableEntity)]
+        public async Task<IActionResult> ObterPokemonPorID(
+            [FromRoute] int id)
+        {
+
+            var resultado = _mapper.Map<PokemonDetalhesViewModel>(await _pokemonService.ObterPokemonPorId(id));
+
+            return CustomResponse(HttpStatusCode.OK, new { resultado });
+        }
+    }
+}
